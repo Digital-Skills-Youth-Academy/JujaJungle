20e4aa2b6ce1cfd43b081943b12c3a5d2248a5f5
Lukas Chapman <lukas.chapman@computingforall.org>
2021-02-01 18:29:57 -0800

    seperate .htmls

---
+++ Grocery.js
@@ -0,0 +1,6 @@
+var myButton= document.getElementById("Da stuf");
+
+myButton.addEventListener('click', function (){}) ;{
+    var MyElement= document.getElementById ('initial');
+    //MyElement.innerHTML="";
+}
