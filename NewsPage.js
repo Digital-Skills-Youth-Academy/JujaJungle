3f5bb1a1a5c26fff84ebd7b9e70e6b3ddebe7be9
alexissan702 <alexissan702@gmail.com>
2021-02-24 20:39:53 -0600

    feb4

--- NewsPage.js
+++ NewsPage.js
@@ -1,14 +1,21 @@
 let ReadMoreButton = document.getElementById('arrow');
 let RemainingArticle = document.getElementById('RemainingArticle');
+let AdjustFlex = document.getElementById('article1');
 
 ReadMoreButton.addEventListener('click', function() {
     //alert("clicked");
-    if (RemainingArticle.style.display == 'block') {
-        RemainingArticle.style.display = 'none';
-        RemainingArticle.style.height = '0px';
+    if (RemainingArticle.classList == 'expandArticle') {
+        RemainingArticle.classList.remove("expandArticle");
+        AdjustFlex.classList.remove("expand");
+        AdjustFlex.classList.add("post");
+        RemainingArticle.classList.add("condense");
+        ReadMoreButton.src = "arrowdown.png";
     }
-    else{
-        RemainingArticle.style.display = 'block';
-        RemainingArticle.style.height = '1000px';
+    else {
+        AdjustFlex.classList.remove("post");
+        RemainingArticle.classList.remove("condense");
+        AdjustFlex.classList.add("expand");
+        RemainingArticle.classList.add("expandArticle");
+        ReadMoreButton.src = "arrowup.png";
     }
 })
