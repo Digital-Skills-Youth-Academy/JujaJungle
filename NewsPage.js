ba19e3efc1c5ec9b9344254c8abbeb5bbe1eac21
alexissan702 <alexissan702@gmail.com>
2021-03-01 20:33:12 -0600

    Added Comment Section

--- NewsPage.js
+++ NewsPage.js
@@ -19,3 +19,41 @@ ReadMoreButton.addEventListener('click', function() {
         ReadMoreButton.src = "arrowup.png";
     }
 })
+
+
+var titles = [];
+var titleInput = document.getElementById("title");
+var messageBox = document.getElementById("display");
+var nameInput = document.getElementById("name");
+function Allow()
+{
+    if (!user.title.value.match(/[a-zA-Z]$/) && user.title.value !="")
+    {
+        user.title.value="";
+        alert("Please Enter only alphabets");
+    }
+    window.location.reload()
+}
+function insert () {
+    titles.push([nameInput.value,titleInput.value]);
+    clearAndShow();
+}
+function clearAndShow ()
+{
+titleInput.value = "";
+nameInput.value="";
+messageBox.innerHTML = "";
+var messages= '';
+    for (var i = 0; i<titles.length; i++) {
+        messages += '<div class="subheader">';
+        messages += titles[i][0];
+        messages += '</div>';
+        messages += '<div class="body">';
+        messages += titles[i][1];
+        messages += '</div>';
+    }
+    messageBox.innerHTML = messages;
+// AdjustFlex.style.height = AdjustFlex.style.height + "100px";
+    
+}
+
