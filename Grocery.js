befd5676bff0c90e3b083d35eaddc7caea3c3073
Lukas Chapman <lukas.chapman@computingforall.org>
2021-03-03 18:24:00 -0800

    Last dedicated work day

--- Grocery.js
+++ Grocery.js
@@ -7,8 +7,41 @@ var plasticUsed//= bagWeight*;
 var button= document.getElementById('newList');
 button.addEventListener('click', draftList)
 
-Element.classList.add("");
+
+var button2= document.getElementById('typeList');
+button2.addEventListener('click', toLists);
+//Element.classList.add("");
+
+
+
+var draftTwo = document.getElementById('listThree');
+var draftList = document.getElementById('listTwo');
+var draftThree = document.getElementById('listOne');
+
+var newList = document.getElementById('typeList');
+
+newList.style.display='none';
+
+
+function toLists() {
+    newList.style.display='none';
+    draftTwo.style.display='block';
+    draftThree.style.display='block';
+    draftList.style.display='block';
+    button.style.display='block';
+    
+}
 
 function draftList() {
-    console.log ('yeah its working!');
+    console.log ('yeah its working!')
+    
+    draftList.style.display = 'none';
+   
+    draftTwo.style.display= 'none';
+
+    draftThree.style.display= 'none';
+
+    button.style.display='none';
+
+    newList.style.display='block';
 }
